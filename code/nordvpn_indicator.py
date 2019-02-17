cf4aecf084e429173e2641d289e4fb4625dafd29
alberto <albe.carde@gmail.com>
2019-02-17 23:23:04 +0000

    Refactor connection status with enum

--- code/nordvpn_indicator.py
+++ code/nordvpn_indicator.py
@@ -18,11 +18,16 @@ gi.require_version('Notify', '0.7')
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 from gi.repository import Notify as notify
+from enum import Enum
 
 
 APPINDICATOR_ID = 'nordvpn_tray_icon'
-TIMER_SECONDS = 10.0
+TIMER_SECONDS = 5.0
 
+class VPN_STATUS(Enum):
+    CONNECTED = 0
+    DISCONNECTED = 1
+    WAITING = 2
 
 class Indicator(object):
     """
@@ -59,25 +64,17 @@ class Indicator(object):
         """
         Sets timer in recurrent loop that executes a VPN status check
         """
+        self.update()
         self.timer = threading.Timer(TIMER_SECONDS, self.status_check_loop)
         self.timer.start()
-        if hasattr(self, 'indicator'):
-            self.nordvpn.status_check(None)
-            self.status_label.set_label(self.nordvpn.get_status())
 
-    def update(self, message, connected):
+    def update(self):
         """
-        Updates the icon and displays a notification if a message is provided
-
-        Args:
-            message: Message for the notification
-            connected: Connected status (True/False)
+        Updates the icon and the menu status item
         """
-        # Removed notifications as the client already push them
-        # if message != None:
-        #     notify.Notification.new("NordVPN", message, None).show()
-        self.indicator.set_icon(self.get_icon_path(connected))
+        self.nordvpn.status_check(None)
         self.status_label.set_label(self.nordvpn.get_status())
+        self.indicator.set_icon(self.get_icon_path(self.nordvpn.is_connected()))
 
     @staticmethod
     def get_icon_path(connected):
@@ -85,9 +82,14 @@ class Indicator(object):
         Returns the correct icon in response to the connection status
 
         Args:
-            connected: Connected status (True/False)
+            connected: Connected status VPN_STATUS object
         """
-        filename = 'nordvpn_connected.png' if connected else 'nordvpn_disconnected.png'
+        if connected == VPN_STATUS.CONNECTED:
+            filename = 'nordvpn_connected.png'
+        elif connected == VPN_STATUS.DISCONNECTED:
+            filename = 'nordvpn_disconnected.png'
+        else:
+            filename = 'nordvpn_waiting.png'
         return os.path.dirname(os.path.realpath(__file__)) + '/' + filename
 
     def build_menu(self):
@@ -108,35 +110,42 @@ class Indicator(object):
 
         menu.append(item_connect)
 
-        countries = self.nordvpn.run_command('nordvpn countries')
+        countries = self.nordvpn.get_countries()
         countries_menu = gtk.Menu()
         item_connect_country = gtk.MenuItem('Countries')
         item_connect_country.set_submenu(countries_menu)
-        if countries is not None:
-            result = ''.join(countries).split(' ')[2].split()
-            for c in result:
-                item = gtk.MenuItem(c)
-                item.connect('activate', self.country_connect_cb)
-                countries_menu.append(item)
+        for c in countries:
+            item = gtk.MenuItem(c)
+            item.connect('activate', self.country_connect_cb)
+            countries_menu.append(item)
         menu_connect.append(item_connect_country)
 
         item_disconnect = gtk.MenuItem('Disconnect')
         item_disconnect.connect('activate', self.nordvpn.disconnect)
         menu.append(item_disconnect)
 
+        # Create a submenu for the connection status
         menu_status = gtk.Menu()
         item_status = gtk.MenuItem('Status')
         item_status.set_submenu(menu_status)
 
-        # First item is to connect automatically
+        # First item is to refresh the status
         item_refresh = gtk.MenuItem('Refresh')
         item_refresh.connect('activate', self.nordvpn.status_check)
         menu_status.append(item_refresh)
 
+        # Add a label to show the current status details
         self.status_label = gtk.MenuItem('')
         menu_status.append(self.status_label)
 
         menu.append(item_status)
+
+        # Create a submenu for the settings
+        menu_settings = gtk.Menu()
+        item_settings = gtk.MenuItem('Settings')
+        item_settings.set_submenu(menu_settings)
+        # TODO add settings
+        menu.append(item_settings)
 
         item_quit = gtk.MenuItem('Quit')
         item_quit.connect('activate', self.quit)
@@ -163,10 +172,20 @@ class Indicator(object):
 
     def auto_connect_cb(self, _):
         """
-        Callback to handle connection to best server
+        Callback to handle connection to auto server
         """
         self.nordvpn.disconnect(None)
         self.nordvpn.connect(None)
+
+    def report(self, message):
+        """
+        Notifies indicator of connection issues and passes a notification
+        message
+
+        Args:
+            message: Message that will be displayed as notification
+        """
+        notify.Notification.new("NordVPN", message, None).show()
 
 
 class NordVPN(object):
@@ -182,7 +201,7 @@ class NordVPN(object):
     """
     def __init__(self):
         self.indicator = None
-        self.connected = False
+        self.connected = VPN_STATUS.WAITING
         self.status = ""
 
     def attach(self, indicator):
@@ -194,16 +213,6 @@ class NordVPN(object):
         """
         indicator.nordvpn = self
         self.indicator = indicator
-
-    def _notify(self, message):
-        """
-        Notifies indicator of connection status and passes a notification
-        message
-
-        Args:
-            message: Message that will be displayed as notification
-        """
-        self.indicator.update(message, self.connected)
 
     def run_command(self, command):
         """
@@ -217,9 +226,8 @@ class NordVPN(object):
         """
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
-        if error != None:
-            self._notify(error)
-
+        if error is not None:
+            self.indicator.report(error)
         return output
 
     def connect(self, _):
@@ -229,15 +237,15 @@ class NordVPN(object):
         Args:
             _: As required by AppIndicator
         """
+        self.connected = VPN_STATUS.WAITING
         output = self.run_command("nordvpn connect")
 
         # Check output
-        if output != None:
+        if output is not None:
             matches = re.search('You are now connected to .*', output)
-            if matches != None:
+            if matches is not None:
                 output = matches.group(0)
-                self.connected = True
-            self._notify(output)
+                self.connected = VPN_STATUS.CONNECTED
 
     def connect_to_country(self, country):
         """
@@ -246,15 +254,15 @@ class NordVPN(object):
         Args:
             country: Country name as string
         """
+        self.connected = VPN_STATUS.WAITING
         output = self.run_command("nordvpn connect {}".format(country))
 
         # Check output
-        if output != None:
+        if output is not None:
             matches = re.search('You are now connected to .*', output)
-            if matches != None:
+            if matches is not None:
                 output = matches.group(0)
-                self.connected = True
-            self._notify(output)
+                self.connected = VPN_STATUS.CONNECTED
 
     def disconnect(self, _):
         """
@@ -263,12 +271,12 @@ class NordVPN(object):
         Args:
             _: As required by AppIndicator
         """
+        self.connected = VPN_STATUS.WAITING
         output = self.run_command("nordvpn disconnect")
-        if output != None:
+        if output is not None:
             matches = re.search('You have been disconnected from NordVPN.', output)
-            if matches != None:
-                self.connected = False
-            self._notify(output)
+            if matches is not None:
+                self.connected = VPN_STATUS.DISCONNECTED
 
     def status_check(self, _):
         """
@@ -281,26 +289,34 @@ class NordVPN(object):
 
         # Get reported NordVPN IP
         output = self.run_command("nordvpn status")
-        if output != None:
-            print output
-            self.status = output.split('-')[1]
+        if output is not None:
+            self.status = output.split('  ')[1].strip()
             matches = re.search(r'(([0-9])+\.){3}([0-9]+)', output)
-            if matches != None:
+            if matches is not None:
                 connected_ip = matches.group(0)
 
-        if connected_ip is None and self.connected is True:
-            self.connected = False
-            self.indicator.update(None, self.connected)
-
-        if connected_ip != None and self.connected is False:
-            self.connected = True
-            self.indicator.update(None, self.connected)
+        self.connected = VPN_STATUS.DISCONNECTED if connected_ip is None else VPN_STATUS.CONNECTED
 
     def get_status(self):
         """
         Returns the current status of the VPN connection as a string
         """
         return self.status
+
+    def is_connected(self):
+        """
+        Returns VPN_STATUS instance representing the connection status
+        """
+        return self.connected
+
+    def get_countries(self):
+        """
+        Returns a list of string representing the available countries
+        """
+        countries = self.run_command('nordvpn countries')
+        if countries is None:
+            return []
+        return ''.join(countries).split(' ')[2].split()
 
 
 def main():
