a6d37a74e39fb2e6bee633e313077809a1c69d16
Yorick van Zweeden <git@yorickvanzweeden.nl>
2019-11-03 19:57:18 +0100

    Make install.sh and uninstall.sh idempotent

--- install.sh
+++ install.sh
@@ -1,59 +1,74 @@
-#!/bin/sh
+#!/bin/bash
 
 install_client()
 {
     # Get NordVPN Repo Setup (https://nordvpn.com/download/linux/)
-    echo Getting the NordVPN Repo Setup
+    echo "Getting the NordVPN Repo Setup"
     wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
 
     # Install downloaded package
-    echo Installing the NordVPN Repo Setup
+    echo "Installing the NordVPN Repo Setup"
     sudo apt-get install -y nordvpn-release_1.0.0_all.deb
     rm nordvpn-release_1.0.0_all.deb
 
     # Update packages
-    echo Updating packages
+    echo "Updating packages"
     sudo apt-get update
 
     # Install NordVPN
-    echo Installing NordVPN
+    echo "Installing NordVPN"
     sudo apt-get install -y nordvpn
 
     # Prompting user to login
-    echo Logging into NordVPN
+    echo "Logging into NordVPN"
     nordvpn login
 }
 
 install_deps()
 {
     # Install gir1.2-appindicator
-    echo Installing AppIndicator
+    echo "Installing AppIndicator and Python-GI"
     sudo apt-get install -y gir1.2-appindicator python-gi
 }
 
 install_indicator()
 {
     # Installing indicator in opt directory
-    echo Installing Ubuntu NordVPN Indicator
+    echo "Installing Ubuntu NordVPN Indicator"
     sudo mkdir -p /opt/ubuntu-nordvpn-indicator/
     sudo cp code/* /opt/ubuntu-nordvpn-indicator/
 
     # Installing autostart desktop file
-    echo Making sure the indicator starts at boot using autostart
+    echo "Making sure the indicator starts at boot using autostart"
     mkdir -p $HOME/.config/autostart
     cp ubuntu-nordvpn-indicator.desktop $HOME/.config/autostart/
 }
 
+# Install client if not present
 if ! command -v nordvpn > /dev/null 2>&1;
 then
     install_client
 fi
 
-install_deps
+# Install dependencies if not present
+deps_available=$(dpkg -l | grep -E "gir1.2-appindicator-|python-gi"  | wc --lines)
+if [[ $deps_available -eq 2 ]]
+then
+    echo "Dependencies have been installed"
+else
+    install_deps
+fi
+
+# Install indicator
 install_indicator
 
 # Starting script
-echo Starting indicator
-nohup $(command -v python) /opt/ubuntu-nordvpn-indicator/nordvpn_indicator.py >/dev/null 2>&1 &
+if pgrep -f "nordvpn_indicator" > /dev/null
+then
+    echo "Indicator already running"
+else
+    echo "Starting indicator"
+    nohup $(command -v python) /opt/ubuntu-nordvpn-indicator/nordvpn_indicator.py >/dev/null 2>&1 &
+fi
 
-echo Finished
+echo "Finished"
--- uninstall.sh
+++ uninstall.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-echo Removing Ubuntu NordVPN Indicator
+echo "Removing Ubuntu NordVPN Indicator"
 rm ~/.config/autostart/ubuntu-nordvpn-indicator.desktop
 sudo rm -rf /opt/ubuntu-nordvpn-indicator
+
+read -p "Do you want to uninstall AppIndicator? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Uninstalling AppIndicator"
+    sudo apt-get remove -y gir1.2-appindicator
+fi
+
+read -p "Do you want to uninstall Python-GI? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Uninstalling Python-GI"
+    sudo apt-get remove -y python-gi
+fi
+
 
 read -p "Do you want to uninstall NordVPN? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo Disconnecting NordVPN
+    echo "Disconnecting NordVPN"
     nordvpn disconnect
 
-    echo Uninstalling NordVPN
+    echo "Uninstalling NordVPN"
     sudo apt-get remove -y nordvpn
 fi
